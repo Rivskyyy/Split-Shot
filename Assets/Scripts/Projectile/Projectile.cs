714fa3c8c32c3033d82d183a79c9e896121da9a5
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2026-04-02 06:15:15 +0300

    fixed explodes for projectile

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -4,24 +4,47 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float explosionRadius = 5f;
 
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        Explode();
+        Destroy(gameObject, 5f);
     }
 
-    private void Explode()
-    {
-        Collider[] victims = Physics.OverlapSphere(transform.position, explosionRadius);
 
-        foreach (Collider victim in victims)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Plane"))
         {
-            if (victim.CompareTag("Obstacle"))
+            return;
+        }
+        if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Explode();
+        }
+        
+    }
+
+   private void Explode()
+    {
+   
+        float dynamicRadius = transform.localScale.x * 2f;
+
+        // ¬¿∆À»¬Œ: œÂÂ‰‡∫ÏÓ Ò‡ÏÂ dynamicRadius Û OverlapSphere
+        Collider[] obstacles = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider col in obstacles)
+        {
+            Renderer rend = col.GetComponent<Renderer>();
+            if (col.CompareTag("Obstacle"))
             {
-                
-                Destroy(victim.gameObject);
+                if(rend != null)
+                {
+                    rend.material.color = Color.red;
+                }
+                Destroy(col.gameObject, 0.3f);
             }
         }
 
+        Destroy(gameObject);
     }
 
 
