9442f7b73a1728d13c2572ed6fcafe30701b29e1
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2026-04-02 10:07:03 +0300

    fixed GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("UI Setup")]
+    [SerializeField] private GameObject generalUI;   
+    [SerializeField] private GameObject gameOverUI;  
+    [SerializeField] private GameObject levelCompletedUI;       
+
+    public static GameManager Instance { get; private set; }
+
+    private bool canRestart = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        Application.targetFrameRate = 60;
+
+        if (generalUI != null) generalUI.SetActive(false);
+        if (gameOverUI != null) gameOverUI.SetActive(false);
+        if (levelCompletedUI != null) levelCompletedUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+       
+        if (canRestart && Input.GetMouseButtonDown(0))
+        {
+            RestartLevel();
+        }
+    }
+
+    public void ShowGameOver()
+    {
+        if (generalUI != null)
+        {
+            generalUI.SetActive(true);     
+            gameOverUI.SetActive(true); 
+            levelCompletedUI.SetActive(false);    
+
+            canRestart = true;
+        }
+    }
+
+    public void ShowWin()
+    {
+        if (generalUI != null)
+        {
+            generalUI.SetActive(true);      
+            levelCompletedUI.SetActive(true);     
+            gameOverUI.SetActive(false); 
+
+            canRestart = true;
+        }
+    }
+
+    public void RestartLevel()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Assets/Scripts/Level/LevelGoal.cs
+++ Assets/Scripts/Level/LevelGoal.cs
@@ -4,16 +4,35 @@ public class LevelGoal : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        
+        if (other.CompareTag("Player"))
         {
-            CompleteLevel();
+            CompleteLevel(other.gameObject);
         }
     }
 
-    private void CompleteLevel()
+    private void CompleteLevel(GameObject player)
     {
         Debug.Log("Level Passed!");
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+      
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ShowWin();
+        }
+
+      
+        PlayerController pc = player.GetComponent<PlayerController>();
+        if (pc != null)
+        {
+            pc.enabled = false;
+        }
+
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 }
